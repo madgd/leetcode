668f0837c949792df800abd056ef1e7c43497d15
madgd <yichizhang@tju.edu.cn>
2016-03-28 21:10:36 +0800

    add some other solutions

--- python/191.py
+++ python/191.py
@@ -1,3 +1,4 @@
+#using bit operations
 class Solution1(object):
 	def hammingweight(self, n):
 		"""
@@ -9,3 +10,25 @@ class Solution1(object):
 			count = count + 1
 			n = (n - 1) & n
 		return count
+
+#math
+class Solution2(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        m = 0
+        while n!=0:
+            m += n % 2
+            n = n / 2
+        return m
+
+#convert to list        
+class Solution3(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return list(bin(n)).count('1')
