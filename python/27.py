bc334d11b9fd318cac2978bcb0f69ee67149f47b
madgd <iamnushead@gmail.com>
2016-04-22 11:20:27 +0800

    update 27.py

--- python/27.py
+++ python/27.py
@@ -9,3 +9,36 @@ class Solution1(object):
             nums.remove(val)
         return len(nums)
 
+class Solution2(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j = j + 1
+        #for i in range(j,len(nums)):
+            #nums.pop()
+        return j
+class Solution3(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                n = n - 1
+            else:
+                i = i + 1
+        #for n in range(j,len(nums)):
+            #nums.pop()
+        return n
