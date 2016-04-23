ccd97a228e5c23c3f370110f2d2dec3de37e1484
madgd <iamnushead@gmail.com>
2016-04-23 17:53:43 +0800

    add 299.py

---
+++ python/299.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+
+        bulls = sum(map(operator.eq, secret, guess))
+        both = sum(min(secret.count(x), guess.count(x)) for x in '0123456789')
+        return '%dA%dB' % (bulls, both - bulls)
