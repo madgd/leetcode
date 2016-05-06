ba752ac6428117e5422ada6d4b32c17298288751
madgd <iamnushead@gmail.com>
2016-05-06 16:48:13 +0800

    add 120.py

--- python/120.py
+++ python/120.py
@@ -12,3 +12,16 @@ class Solution1(object):
                 sumList[j] = min(sumList[j], sumList[j - 1]) + triangle[i][j]
             sumList[0] += triangle[i][0]
         return min(sumList)
+
+class Solution2(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        #自底向上递推
+        sumList = triangle[-1]
+        for i in xrange(len(triangle)-2, -1, -1):
+            for j in xrange(0, i+1):
+                sumList[j] = min(sumList[j], sumList[j + 1]) + triangle[i][j]
+        return sumList[0]
